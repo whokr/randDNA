17657a7708e7cf10a9be21fd543fc4a0f539435c
whokr <whokr@users.noreply.github.com>
2019-02-14 16:24:21 -0400

    lab2

---
+++ src/rdna.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+	string randDNA(int seed, string bases, int length)
+	{
+		string randDNA = "";
+		mt19937 eng1 (seed);
+		uniform_int_distribution<int> un(0, bases.size()-1);
+		
+		if (bases.size() == 0)
+		{
+				answer = "";
+				return answer;
+		}
+		
+		for(int i=0; i< length; i++)
+		{
+				int ans = un(eng1);
+				answer += bases[ans];
+		}
+				return answer;
+	}
+		
+		
+		
